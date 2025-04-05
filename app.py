cac75ebfac89f4fcbf99cdae56b9228e67f222da
philiphuynh1 <156853689+philiphuynh1@users.noreply.github.com>
2025-04-05 02:17:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import pandas as pd
-import os
+import math
+
+
 
 pg = st.navigation([
     st.Page("pgTitle.py", title="Title", icon=":material/favorite:"),
@@ -9,20 +10,6 @@ pg = st.navigation([
 ], position='hidden')
 pg.run()
 
-@st.cache_data
-def load_grouped_data():
-    csv_file = "Grouped_Data.csv"
-    # Check if the CSV file exists; if not, convert the Excel file.
-    if not os.path.exists(csv_file):
-        df = pd.read_excel("Grouped_Data.xlsx")
-        df.to_csv(csv_file, index=False)
-    else:
-        df = pd.read_csv(csv_file)
-    return df
 
-# Load data (this is now cached and will only re-run if the file changes)
-data = load_grouped_data()
 
-st.title("My Streamlit App")
-st.write("Data loaded from Grouped_Data.csv:")
-st.dataframe(data)
+
